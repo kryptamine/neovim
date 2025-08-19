3f33d58f9199fe543310e58eeca49f07f958941e
Alexander Satretdinov <satretdinovalexander@gmail.com>
2025-08-19 12:38:30 +0300

    update conform

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,3 +1,14 @@
+local function first(bufnr, ...)
+  local conform = require 'conform'
+  for i = 1, select('#', ...) do
+    local formatter = select(i, ...)
+    if conform.get_formatter_info(formatter, bufnr).available then
+      return formatter
+    end
+  end
+  return select(1, ...)
+end
+
 local prettier_langs = {
   'javascript',
   'typescript',
@@ -23,11 +34,24 @@ local options = {
     go = { 'gofmt' },
   },
 
+  default_format_opts = {
+    lsp_format = 'fallback',
+  },
+
   formatters = {},
 }
 
 for _, lang in ipairs(prettier_langs) do
   options.formatters_by_ft[lang] = { 'prettier' }
+end
+
+-- Override JS/TS to prefer eslint if available
+options.formatters_by_ft.javascript = function(bufnr)
+  return { first(bufnr, 'prettierd', 'prettier'), first(bufnr, 'eslint_d', 'eslint') }
+end
+
+options.formatters_by_ft.typescript = function(bufnr)
+  return { first(bufnr, 'prettierd', 'prettier'), first(bufnr, 'eslint_d', 'eslint') }
 end
 
 return {
