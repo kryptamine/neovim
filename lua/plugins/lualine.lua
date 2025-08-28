970c5e3483a4b615eb24da47890ed0838c0c58e6
Alexander Satretdinov <satretdinovalexander@gmail.com>
2025-08-28 12:56:17 +0300

    replace lualine -> slimline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,63 +1,30 @@
 return {
   {
-    'nvim-lualine/lualine.nvim',
-    enabled = true,
-    lazy = false,
-    event = { 'BufReadPost', 'BufNewFile', 'VeryLazy' },
-    opts = function()
-      return {
-        options = {
-          theme = 'auto',
-          globalstatus = true,
-          disabled_filetypes = {
-            statusline = { 'dashboard', 'alpha', 'starter', 'snacks_dashboard' },
-          },
-          section_separators = { left = '', right = '' },
-          component_separators = { left = '', right = '' },
+    'sschleemilch/slimline.nvim',
+    opts = {
+      spaces = {
+        components = '',
+        left = '',
+        right = '',
+      },
+      sep = {
+        hide = {
+          first = true,
+          last = true,
         },
-        sections = {
-          lualine_a = {
-            {
-              function()
-                return ''
-              end,
-              padding = { left = 1, right = 0 },
-              separator = '',
-            },
-            'mode',
-          },
-          lualine_b = {
-            'branch',
-            'diff',
-            {
-              'filename',
-              path = 1,
-              padding = { left = 1, right = 1 },
-              fmt = function(name)
-                local devicons = require 'nvim-web-devicons'
-
-                local icon, _ = devicons.get_icon(name, nil, { default = true })
-                return icon .. ' ' .. name
-              end,
-            },
-          },
-          lualine_c = {
-            { 'diagnostics', sources = { 'nvim_lsp' } },
-            { 'searchcount' },
-          },
-          lualine_x = {},
-          lualine_y = { 'progress' },
-          lualine_z = {
-            { 'location', separator = '' },
-            {
-              function()
-                return ''
-              end,
-              padding = { left = 0, right = 1 },
-            },
+        left = '',
+        right = '',
+      },
+      configs = {
+        path = {
+          trunc_width = 40,
+          directory = true,
+          truncate = {
+            chars = 1,
+            full_dirs = 3,
           },
         },
-      }
-    end,
+      },
+    },
   },
 }
