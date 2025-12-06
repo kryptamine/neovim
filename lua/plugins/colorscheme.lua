7383f88da43596e8f7a9ab4e14867068bc2505af
Alexander Satretdinov <satretdinovalexander@gmail.com>
2025-12-06 16:59:38 +0200

    install kulala

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -35,6 +35,7 @@ return {
   -- },
   {
     'sainnhe/gruvbox-material',
+    priority = 1000,
     config = function()
       vim.g.gruvbox_material_foreground = 'mix'
       vim.g.gruvbox_material_background = 'hard'
